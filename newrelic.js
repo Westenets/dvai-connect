0368dce8dafcda50e44237002fa2d120051f1506
Deep Chakraborty <chakraborty.deep013@gmail.com>
2026-03-06 13:59:52 +0530

    Update newrelic.js

--- newrelic.js
+++ newrelic.js
@@ -3,8 +3,18 @@ require('dotenv').config();
 module.exports = {
     app_name: [process.env.NEW_RELIC_APP_NAME],
     license_key: process.env.NEW_RELIC_LICENSE_KEY,
-    logging: {
+    agent_enabled: process.env.NODE_ENV === 'production',
+    ai_monitoring: {
         enabled: true,
+    },
+    distributed_tracing: {
+        enabled: true,
+    },
+    transaction_tracer: {
+        enabled: true,
+    },
+    logging: {
+        enabled: process.env.NODE_ENV === 'production',
         level: 'info',
     },
 };
